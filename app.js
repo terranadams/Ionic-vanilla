5bb4253ef0f1ae9054cb9b58dc7d5093a7f2e739
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2023-01-02 17:48:12 -0700

    validating user input

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const reasonInput = document.querySelector('#input-reason')
-const amountInput = document.querySelector('#input-amount')
-const cancelBtn = document.querySelector('#btn-cancel')
-const confirmBtn = document.querySelector('#btn-confirm')
+const reasonInput = document.querySelector("#input-reason");
+const amountInput = document.querySelector("#input-amount");
+const cancelBtn = document.querySelector("#btn-cancel");
+const confirmBtn = document.querySelector("#btn-confirm");
 
-confirmBtn.addEventListener('click', () => {
-    console.log('confirmed')
-})
-cancelBtn.addEventListener('click', () => {
-    console.log('cancelled')
-})
+confirmBtn.addEventListener("click", () => {
+  // console.log('confirmed')
+  const enteredReason = reasonInput.value;
+  const enteredAmount = amountInput.value;
+
+  // validation logic
+  if (
+    enteredReason.trim().length <= 0 ||
+    enteredAmount <= 0 ||
+    enteredAmount.trim().length <= 0
+  ) {
+    return;
+  }
+  console.log("Reason: " + enteredReason);
+  console.log("Amount: " + enteredAmount);
+});
+
+cancelBtn.addEventListener("click", () => {
+  // console.log('cancelled')
+});
